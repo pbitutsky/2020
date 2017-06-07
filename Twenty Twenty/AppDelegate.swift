55a01ba3aeb724a8fa0e34cf842351d30fc2c3ca
Paul Bitutsky <pbitutsky@berkeley.edu>
2017-06-06 21:08:50 -0400

    Initial Commit

---
+++ Twenty Twenty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twenty Twenty
+//
+//  Created by Paul Bitutsky on 6/6/17.
+//  Copyright © 2017 Paul Bitutsky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Twenty Twenty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twenty Twenty
+//
+//  Created by Paul Bitutsky on 6/6/17.
+//  Copyright © 2017 Paul Bitutsky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
